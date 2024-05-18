29005f512929335a93c0f539ac13fd32f494bd6a
moatazmohamed <mohamed.moataz194@gmail.com>
2024-05-18 19:40:59 +0300

    first

---
+++ android/app/src/main/kotlin/com/example/test_download/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_download
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,232 @@
+import 'dart:io';
+
+import 'package:download_assets/download_assets.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Download Assets Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'Download Assets'),
+      );
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({required this.title});
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DownloadAssetsController downloadAssetsController =
+      DownloadAssetsController();
+  DownloadAssetsController downloadAssetsController0 =
+      DownloadAssetsController();
+  String message = 'Press the download button to start the download';
+  bool downloaded = false;
+  double value = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future _init() async {
+    await downloadAssetsController.init(assetDir: 'math');
+    downloaded = await downloadAssetsController.assetsDirAlreadyExists();
+    print('${downloadAssetsController.assetsDir}/assessment_part.png');
+    print(
+        "$downloaded , ${downloadAssetsController.assetsDir}, ${await downloadAssetsController.assetsDirAlreadyExists()}");
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (downloaded) ...[
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: FileImage(File(
+                          '${downloadAssetsController.assetsDir}/assessment_part.png')),
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: FileImage(File(
+                          '${downloadAssetsController.assetsDir}/assessment_part.png')),
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                ),
+              ],
+              TweenAnimationBuilder<double>(
+                duration: const Duration(milliseconds: 250),
+                curve: Curves.easeInOut,
+                tween: Tween<double>(
+                  begin: 0,
+                  end: value,
+                ),
+                builder: (context, value, _) => LinearProgressIndicator(
+                  minHeight: 10,
+                  value: value,
+                ),
+              ),
+              Text(
+                message,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: _downloadAssets,
+              tooltip: 'Download',
+              child: Icon(Icons.arrow_downward),
+            ),
+            const SizedBox(
+              width: 25,
+            ),
+            FloatingActionButton(
+              onPressed: () async {
+                await downloadAssetsController.clearAssets();
+                await _downloadAssets();
+              },
+              tooltip: 'Refresh',
+              child: Icon(Icons.refresh),
+            ),
+            const SizedBox(
+              width: 25,
+            ),
+            FloatingActionButton(
+              onPressed: _cancel,
+              tooltip: 'Cancel',
+              child: Icon(Icons.cancel_outlined),
+            ),
+          ],
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+
+  Future _downloadAssets() async {
+    // final assetsDownloaded =
+    //     await downloadAssetsController.assetsFileExists('math');
+    // print('assetsDownloaded:$assetsDownloaded');
+    // if (assetsDownloaded) {
+    //   setState(() {
+    //     message = 'Click in refresh button to force download';
+    //     print(message);
+    //   });
+    //   return;
+    // }
+
+    try {
+      value = 0.0;
+      downloaded = false;
+      await downloadAssetsController.init(assetDir: 'math');
+
+      await downloadAssetsController.startDownload(
+          onCancel: () {
+            message = 'Cancelled by user';
+            setState(() {});
+          },
+          assetsUrls: [
+            'https://github.com/edjostenes/download_assets/raw/main/download/assets.zip',
+          ],
+          onProgress: (progressValue) {
+            value = progressValue;
+            setState(() {
+              message =
+                  'Downloading - ${(progressValue * 100).toStringAsFixed(2)}';
+              print(message);
+            });
+          },
+          onDone: () {
+            setState(() {
+              downloaded = true;
+              message =
+                  'Download completed\nClick in refresh button to force download';
+            });
+          });
+    } on DownloadAssetsException catch (e) {
+      print(e.toString());
+      setState(() {
+        downloaded = false;
+        message = 'Error: ${e.toString()}';
+      });
+    }
+    print('${downloadAssetsController.assetsDir}');
+    print('${downloadAssetsController.assetsDir?.length}');
+    print("${await CounterStorage()._localPath}");
+    print(
+        "${await CounterStorage().listFilesAndFolders(downloadAssetsController.assetsDir ?? '')}");
+  }
+
+  void _cancel() => downloadAssetsController.cancelDownload();
+}
+
+class CounterStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/math/');
+  }
+
+  Future<List<String>> listFilesAndFolders(String path) async {
+    final directory = Directory(path);
+    if (await directory.exists()) {
+      final filesAndFolders = directory.listSync();
+      return filesAndFolders.map((e) => e.path).toList();
+    } else {
+      return [];
+    }
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+      print('$file');
+      // Read the file
+      print(await file.length());
+      final contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+}
